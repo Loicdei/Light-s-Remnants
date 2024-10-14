2f9c9030aadf45baaccc1fbe7b181c481e17c412
loicd <loic.deiva@outlook.fr>
2024-10-14 19:17:06 +0200

    Feat : checkpoint + playerSpawn + DeathZone

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Transform playerSpawn;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerSpawn.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
+        
