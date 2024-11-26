ec8f8318ef33094524a5231781612d1a43536fad
leo <leo.esnault@etu.u-paris.fr>
2024-11-26 13:10:28 +0100

    création préfab

--- Assets/Scripts/deplacementEnnemi.cs
+++ Assets/Scripts/deplacementEnnemi.cs
@@ -31,20 +31,12 @@ public class deplacementEnnemi : MonoBehaviour
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
         {
-            flip();
             currentPoint = pointA.transform;
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
         {
-            flip();
             currentPoint = pointB.transform;
         }
-    }
-    private void flip()
-    {
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1;
-        transform.localScale = localScale;
     }
     private void OnDrawGizmos()
     {
