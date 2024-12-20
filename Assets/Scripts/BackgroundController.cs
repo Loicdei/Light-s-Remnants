739061858a886d10c3e8271378389a8646b03f08
olltrack <oleghiegel@gmail.com>
2024-12-20 11:27:48 +0100

    feat : infinite scrolling background

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class BackgroundConroller : MonoBehaviour
 {
-    private float startPos;
+    private float startPos, length;
     public GameObject cam;
     public float parallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float distance = cam.transform.position.x * parallaxEffect; //0 = bouge avec la cam, 1 = reste fixe, 0.5 = moitié de la vitesse de la cam
+        float movement = cam.transform.position.x * ( 1 - parallaxEffect );
 
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        //ajustement de position pour scrolling infini du bg
+        if (movement > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
     }
 }
