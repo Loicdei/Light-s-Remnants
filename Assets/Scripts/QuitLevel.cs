8819bf7f71a3558e9fe2511993488e9cea21b2d2
Sowenpro <cardonsowenpro@gmail.com>
2025-01-12 19:29:14 +0100

    feat : ajout de la transition lors du QuitLevel

--- Assets/Scripts/QuitLevel.cs
+++ Assets/Scripts/QuitLevel.cs
@@ -1,13 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitLevel : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private bool playerInRange;
-    // Update is called once per frame
+    private Animator fadeSystem;
+    Rigidbody2D playerRb;
+    private PlayerController playerController;
+    private GrabController grabController;
+
+    private void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        grabController = GameObject.FindGameObjectWithTag("Player").GetComponent<GrabController>();
+        playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
     void Start()
     {
         BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
@@ -19,7 +33,7 @@ public class QuitLevel : MonoBehaviour
             // Assuming you have a way to check if the player is in range
             if (playerInRange)
             {
-                UnityEngine.SceneManagement.SceneManager.LoadScene("testlevel");
+                StartCoroutine(TransitionLevel());
             }
         }
     }
@@ -37,6 +51,32 @@ public class QuitLevel : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             playerInRange = false;
+        }
+    }
+    private bool isTransitioning = false;
+
+    private IEnumerator TransitionLevel()
+    {
+        if (isTransitioning) yield break;
+        isTransitioning = true;
+
+        if (playerController != null)
+        {
+            playerController.SetPauseState(true);
+            grabController.SetPauseState(true);
+        }
+        playerRb.simulated = false;
+        Time.timeScale = 0;
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync("MenuJouable");
+        yield return new WaitForSecondsRealtime(1f);
+        playerRb.simulated = true;
+        if (playerController != null)
+        {
+            playerController.SetPauseState(false);
+            grabController.SetPauseState(false);
         }
     }
 }
