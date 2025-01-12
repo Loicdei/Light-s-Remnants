37c722b5962054e550aa5f093757b11d821e67bf
Sowenpro <cardonsowenpro@gmail.com>
2025-01-12 19:14:00 +0100

    feat : ajout transition doors
    + les niveaux correspondent à leurs portes

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -9,11 +9,24 @@ public class Door : MonoBehaviour
     [SerializeField] Sprite SpriteDoorLocked;
     [SerializeField] Sprite SpriteDoorClosed;
     [SerializeField] SceneAsset scene;
+    private Animator fadeSystem;
 
     private bool playerInRange = false;
     public bool isDoorUnlocked = false;
     private SpriteRenderer spriteRenderer;
 
+    private bool isTransitioning = false;
+    Rigidbody2D playerRb;
+    private PlayerController playerController;
+    private GrabController grabController;
+
+    private void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        grabController = GameObject.FindGameObjectWithTag("Player").GetComponent<GrabController>();
+        playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -27,12 +40,37 @@ public class Door : MonoBehaviour
         {
             if (playerInRange)
             {
-                SceneManager.LoadScene(scene.name);
+                StartCoroutine(OpenDoor());
             }
             else
             {
             // Animation Shake door + sound effect
             }
+        }
+    }
+
+    private System.Collections.IEnumerator OpenDoor()
+    {
+        if (isTransitioning) yield break;
+        isTransitioning = true;
+
+        if (playerController != null)
+        {
+            playerController.SetPauseState(true);
+            grabController.SetPauseState(true);
+        }
+        playerRb.simulated = false;
+        Time.timeScale = 0;
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(scene.name);
+        yield return new WaitForSecondsRealtime(1f);
+        playerRb.simulated = true;
+        if (playerController != null)
+        {
+            playerController.SetPauseState(false);
+            grabController.SetPauseState(false);
         }
     }
 
