0d33fdfb7c9a7b955c7e40b55a2b1a5b63800585
Charles Bouvier <charlesbouvier832@hotmail.com>
2025-01-12 17:17:54 +0100

    Feat : something

--- Assets/Scripts/FinalLevel.cs
+++ Assets/Scripts/FinalLevel.cs
@@ -5,34 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class FinalLevel : MonoBehaviour
 {
-    [SerializeField] private SceneAsset targetScene; // Scène cible pour la transition
-    [SerializeField] private UnityEngine.Rendering.Universal.Light2D transitionLight; // Lumière pour l'effet
-    [SerializeField] private float lightIntensityTarget = 1.5f; // Intensité cible de la lumière
-    [SerializeField] private float lightRadiusTarget = 5f; // Rayon cible de la lumière
-    [SerializeField] private float lightTransitionSpeed = 1f; // Vitesse de transition de la lumière
+    [SerializeField] private SceneAsset targetScene; // Scï¿½ne cible pour la transition
+    [SerializeField] private UnityEngine.Rendering.Universal.Light2D transitionLight; // Lumiï¿½re pour l'effet
+    [SerializeField] GrabController grabController;
+
 
     private bool playerInRange = false;
     private bool isTransitioning = false;
 
     void Start()
     {
-        // Initialisation de la lumière à faible intensité
+        // Initialisation de la lumiï¿½re ï¿½ faible intensitï¿½
         transitionLight.intensity = 0f;
         transitionLight.pointLightOuterRadius = 0f;
     }
 
     void Update()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E) && !isTransitioning)
+        if (playerInRange && grabController.isHoldingLantern() && Input.GetKeyDown(KeyCode.E) && !isTransitioning)
         {
             StartCoroutine(TransitionLevel());
         }
 
-        // Transition progressive de la lumière si elle est en cours de transition
+        // Transition progressive de la lumiï¿½re si elle est en cours de transition
         if (isTransitioning)
         {
-            transitionLight.intensity = Mathf.Lerp(transitionLight.intensity, lightIntensityTarget, Time.deltaTime * lightTransitionSpeed);
-            transitionLight.pointLightOuterRadius = Mathf.Lerp(transitionLight.pointLightOuterRadius, lightRadiusTarget, Time.deltaTime * lightTransitionSpeed);
+            transitionLight.intensity = Mathf.Lerp(transitionLight.intensity, 500f, Time.deltaTime * 1);
+            transitionLight.pointLightOuterRadius = Mathf.Lerp(transitionLight.pointLightOuterRadius, 500f, Time.deltaTime * 1);
         }
     }
 
@@ -56,8 +55,8 @@ public class FinalLevel : MonoBehaviour
     {
         isTransitioning = true;
 
-        // Début de la séquence de fade
+        // Dï¿½but de la sï¿½quence de fade
         yield return new WaitForSecondsRealtime(1f); // Attend la fin de l'animation de fade
-        SceneManager.LoadSceneAsync(targetScene.name); // Charge la nouvelle scène
+        SceneManager.LoadSceneAsync(targetScene.name); // Charge la nouvelle scï¿½ne
     }
 }
