a9b05ec3e50fe5ee72892d23ea07fb0da9cff44a
Sowenpro <cardonsowenpro@gmail.com>
2025-01-12 17:54:31 +0100

    Feat : script SelectOnHover
    Ajout d'un script pour changer la selection d'un bouton à la souris

---
+++ Assets/Scripts/SelectOnHover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectOnHover : MonoBehaviour, IPointerEnterHandler
+{
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (button != null && button.interactable)
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+}
