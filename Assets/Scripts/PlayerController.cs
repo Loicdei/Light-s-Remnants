efc32dfb268a0a9dc1ec452a5cf21f99622ab241
Charles Bouvier <charlesbouvier832@hotmail.com>
2025-02-20 23:29:40 +0100

    feat : things

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,14 +76,15 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        joystick.gameObject.SetActive(true);
-        joystickBG.gameObject.SetActive(true);
-        jumpButton.gameObject.SetActive(true);
-        lanternButton.gameObject.SetActive(true);
-        pauseButton.gameObject.SetActive(true);
-        beaconButton.gameObject.SetActive(true);
-        xInput = joystick.Horizontal();
-
+        /*
+                joystick.gameObject.SetActive(true);
+                joystickBG.gameObject.SetActive(true);
+                jumpButton.gameObject.SetActive(true);
+                lanternButton.gameObject.SetActive(true);
+                pauseButton.gameObject.SetActive(true);
+                beaconButton.gameObject.SetActive(true);
+                xInput = joystick.Horizontal();
+        */
 
         isGrounded = groundCheck.IsTouchingLayers(groundLayer);
         //Coyote time
